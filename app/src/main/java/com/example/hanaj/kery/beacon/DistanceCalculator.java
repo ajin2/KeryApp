6e39bebdf0a1bd2ea3be30a2cc14378ec5c05eb2
DESKTOP-I2L3DS6\tnghk <ehrms4611@gmail.com>
2017-10-10 16:05:32 +0900

    Revert "no message"
    This reverts commit 09e719c7f00b6c381f6f9d11bbf6f4e8ea3d5b0a.

--- app/src/main/java/com/example/hanaj/kery/beacon/DistanceCalculator.java
+++ app/src/main/java/com/example/hanaj/kery/beacon/DistanceCalculator.java
@@ -2,6 +2,7 @@ package com.example.hanaj.kery.beacon;
 
 
 import android.util.Log;
+
 /**
  * Created by tnghk on 2017-08-21.
  */
@@ -14,12 +15,7 @@ public class DistanceCalculator {
     private static String infrareDirection="";
     private static String direction="";
     private static String total;
-    private static String speed;
-
-    private Kalman mKalmanAccLeft = new Kalman(0.0f);
-    private Kalman mKalmanAccCenter = new Kalman(0.0f);
-    private Kalman mKalmanAccRight = new Kalman(0.0f);
-
+    private String speed;
     //비콘과 센서를 통합하여 방향판단하는 쓰레드
     public synchronized void calculate(){
         beaconCalculate();
@@ -35,21 +31,17 @@ public class DistanceCalculator {
         /*beaconDirection = beaconInfo.getLeft_distance() > beaconInfo.getCenter_distance() ?
                 beaconInfo.getCenter_distance()>beaconInfo.getRight_distance() ? "13" : "12" : "11";
         */
-        float filteredLeft = 0.0f;
-        float filteredCenter = 0.0f;
-        float filteredRight = 0.0f;
+        int left=beaconInfo.getLeft_distance();
+        int center=beaconInfo.getCenter_distance();
+        int right=beaconInfo.getRight_distance();
 
-        filteredLeft = (float) mKalmanAccLeft.update(beaconInfo.getLeft_rssi());
-        filteredCenter = (float) mKalmanAccCenter.update(beaconInfo.getCenter_rssi());
-        filteredRight = (float) mKalmanAccRight.update(beaconInfo.getRight_rssi());
-
-        if(filteredLeft < filteredRight) {
+        if(left>right) {
             beaconDirection = "13";
-            if(filteredRight < filteredCenter)
+            if(right>center)
                 beaconDirection="12";
         }else {
             beaconDirection = "11";
-            if(filteredLeft < filteredCenter)
+            if(left>center)
                 beaconDirection = "12";
         }
         distanceCalculate();
@@ -65,29 +57,35 @@ public class DistanceCalculator {
         Log.d("Calculator","Drirection->"+total);
         return total;
     }
-    private void distanceCalculate() {
-        if (beaconDirection.equals("13"))
-            beaconDistance = beaconInfo.getRight_rssi();
-        else if (beaconDirection.equals("11"))
-            beaconDistance = beaconInfo.getLeft_rssi();
+    private void distanceCalculate(){
+        if(beaconDirection.equals("13"))
+            beaconDistance=beaconInfo.getRight_distance();
+        else if(beaconDirection.equals("11"))
+            beaconDistance=beaconInfo.getLeft_distance();
         else
-            beaconDistance = beaconInfo.getCenter_rssi();
+            beaconDistance=beaconInfo.getCenter_distance();
 
-        if (beaconDistance >= -59)
-            speed = "0";
-        else if(beaconDistance < -59 &&  beaconDistance >= -62)
-            speed ="1";
-        else if(beaconDistance <-62 && beaconDistance >= -65)
-            speed = "2";
-        else if(beaconDistance <-65 && beaconDistance >=-68)
-            speed = "3";
-        else if(beaconDistance <-68 && beaconDistance >=-71)
-            speed = "4";
-        else if(beaconDistance <-71 && beaconDistance >=-74)
-            speed = "5";
-        else if(beaconDistance < -74 && beaconDistance >=-77)
-            speed = "6";
+        if(beaconDistance<=1000)
+            speed="0";
+        else if(beaconDistance>1000 && beaconDistance<=1200)
+            speed="1";
+        else if(beaconDistance>1200 && beaconDistance<=1700)
+            speed="2";
+        else if(beaconDistance>1700 && beaconDistance<=2500)
+            speed="3";
+        else if(beaconDistance>2500 && beaconDistance<=3500)
+            speed="4";
+        else if(beaconDistance>3500 && beaconDistance<=5000)
+            speed="5";
+        else if(beaconDistance>5000 && beaconDistance<=6000)
+            speed="6";
+        else if(beaconDistance>6000 && beaconDistance<=7000)
+            speed="7";
+        else if(beaconDistance>7000 && beaconDistance<=8000)
+            speed="8";
+        else if(beaconDistance>8000 && beaconDistance<=9000)
+            speed="9";
         else
-            speed = "7";
+            speed="10";
     }
 }
